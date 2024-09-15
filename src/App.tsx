c652df114b76eda2376cfc43b6a7614b018e7997
sutigit <panyanilben@gmail.com>
2024-09-15 18:59:51 +0300

    init push

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import data from './example_data/lotrapi.json'
 
-import SjnDisplay from "./SuperJSON/Display"
+import JSONDisplay from "./SuperJSON/Display"
 // import SjnRowAddOn from "./SuperJSON/RowAddon"
 
 export default function App() {
 
   return (
     <main style={{ padding: 50 }}>
-      <SjnDisplay json={data}>
-        {/* <SjnRowAddOn visibilityRules={}>
-            <p>Hello</p>
-        </SjnRowAddOn> */}
-      </SjnDisplay>
+      <JSONDisplay json={data}>
+      </JSONDisplay>
     </main>
   )
 };
